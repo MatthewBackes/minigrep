de3799783e45c6e4056130ff41b8b2eab2ef7142
Strom-Cuzewon <mbackes5052@gmail.com>
2024-04-27 14:37:17 -0500

    Grep project intial commit.

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Search for {}", query);
+    println!("In {}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Cannot read file.");
+
+    println!("With text:\n{contents}");
+}
